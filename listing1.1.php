649207eeaadc6f4fd7d2911d70094c08eb8f1f18
Maks Surguy <msurguy@gmail.com>
2013-11-03 20:51:11 -0800

    fixed quotes

--- listing1.1.php
+++ listing1.1.php
@@ -1,14 +1,14 @@
-$order = array(‘customer’ => ‘Prince Caspian’, ‘product’ => ‘Laravel in Action’);
-Session::put(‘order’,$order);						
+$order = array('customer' => 'Prince Caspian', 'product' => 'Laravel in Action');
+Session::put('order',$order);						
 
 ...
 
-$orderInDB = Order::create(Session::get(‘order’));
+$orderInDB = Order::create(Session::get('order'));
 
 Session::forget('order');						
 
-$ordersForCaspian = Order::where(‘customer’, ‘Prince Caspian’)->get();
+$ordersForCaspian = Order::where('customer', 'Prince Caspian')->get();
 
 ...
 
-$ordersQuantity = Order::where(‘product’, ‘Laravel in Action’)->count();
+$ordersQuantity = Order::where('product', 'Laravel in Action')->count();
